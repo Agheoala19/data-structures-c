77b6bb0f48fd187f97472161ad3d9cfe54073035
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-07-01 00:04:40 +0300

    Update Stack

--- src/stack.c
+++ src/stack.c
@@ -9,4 +9,49 @@ typedef struct Node Node;
 struct Node {
   int info;
   Node* next;
+};
+
+void push(Node** top, int info) {
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->info = info;
+  newNode->next = *top;
+  *top = newNode;
+}
+
+int pop(Node** top) {
+  if(*top == NULL)
+    return 0;
+
+  int val = (*top)->info;
+  Node* temp = *top;
+  *top = (*top)->next;
+  free(temp);
+
+  return val;
+}
+
+void printStack(Node* top) {
+    if (top == NULL) {
+        printf("Stack is empty.\n");
+        return;
+    }
+
+    printf("Stack contents:\n");
+    while (top != NULL) {
+        printf("%d\n", top->info);
+        top = top->next;
+    }
+}
+
+void main() {
+  Node* stack = NULL;
+
+  push(&stack, 10);
+  push(&stack, 20);
+  push(&stack, 30);
+
+  printf("Pop node: %d\n", pop(&stack)); //will return  30
+  printf("Pop node: %d\n", pop(&stack)); //will return  20
+  printf("Pop node: %d\n", pop(&stack)); //will return  10
+  printf("Pop node: %d\n", pop(&stack)); //will return  0
 }
